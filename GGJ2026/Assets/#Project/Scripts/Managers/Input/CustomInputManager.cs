207f3f9702171403bee1851f4520c7b5a08efcac
GesquiereJeroen <gesquierejeroen@gmail.com>
2026-01-31 11:16:39 +0100

    Player Controller
    Added player controller and prefab
    also imported cinemachine for future camera use

---
+++ GGJ2026/Assets/#Project/Scripts/Managers/Input/CustomInputManager.cs
@@ -0,0 +1,226 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class CustomInputManager
+{
+	#region Enums
+	public enum Player
+	{
+		Move,
+	}
+	public enum InputType
+	{
+		Keyboard,
+		GamePad
+	}
+	#endregion
+
+	#region Fields
+	private List<InputAction> _playerInputActions = new List<InputAction>();
+	private GameInput _inputSystem;
+
+	private bool _lastInputWasController = false;
+	private InputType _currentDeviceType;
+	#endregion
+
+	#region Properties
+	public InputType CurrentDeviceType
+	{
+		get
+		{
+			return _currentDeviceType;
+		}
+		set
+		{
+			if (_currentDeviceType != value)
+			{
+				_currentDeviceType = value;
+				OnDeviceSwitch(_currentDeviceType);
+			}
+		}
+	}
+
+	public event EventHandler<DeviceSwitchEventArgs> DeviceSwitch;
+
+	private static CustomInputManager _instance;
+	public static CustomInputManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = new CustomInputManager();
+			}
+			return _instance;
+		}
+	}
+	#endregion
+
+	#region Constructor
+	private CustomInputManager()
+	{
+		_inputSystem = new GameInput();
+
+		AssignInputActions();
+
+		EnableAll();
+		SetupDeviceType();
+	}
+	#endregion
+
+	#region Methods
+	private void AssignInputActions()
+	{
+		// Player Map
+		_playerInputActions.Add(_inputSystem.Player.Move);
+	}
+
+	private InputAction GetAction(Player inputType)
+	{
+		return _playerInputActions[(int)inputType];
+	}
+
+	private void EnableAction(Player input)
+	{
+		_playerInputActions[(int)input].Enable();
+	}
+	private void DisableAction(Player input)
+	{
+		_playerInputActions[(int)input].Disable();
+	}
+	private void EnableAllPlayer()
+	{
+		foreach (var action in _playerInputActions)
+		{
+			action.Enable();
+		}
+	}
+	private void DisableAllPlayer()
+	{
+		foreach (var action in _playerInputActions)
+		{
+			action.Disable();
+		}
+	}
+
+	private void EnableAll()
+	{
+		EnableAllPlayer();
+	}
+	private void DisableAll()
+	{
+		DisableAllPlayer();
+	}
+
+	#region Events
+
+	private void SubscribeOnPerform(Player input, Action<InputAction.CallbackContext> function)
+	{
+		_playerInputActions[(int)input].performed += function;
+	}
+	private void UnsubscribeOnPerform(Player input, Action<InputAction.CallbackContext> function)
+	{
+		_playerInputActions[(int)input].performed -= function;
+	}
+	private void SubscribeOnCancel(Player input, Action<InputAction.CallbackContext> function)
+	{
+		_playerInputActions[(int)input].canceled += function;
+	}
+	private void UnsubscribeOnCancel(Player input, Action<InputAction.CallbackContext> function)
+	{
+		_playerInputActions[(int)input].canceled -= function;
+	}
+	#endregion
+
+	#region Static Methods
+	public static InputAction GetInputAction(Player inputType)
+	{
+		return Instance.GetAction(inputType);
+	}
+
+	public static void EnableInputAction(Player input)
+	{
+		Instance.EnableAction(input);
+	}
+	public static void DisableInputAction(Player input)
+	{
+		Instance.DisableAction(input);
+	}
+	public static void EnableAllPlayerActions()
+	{
+		Instance.EnableAllPlayer();
+	}
+	public static void DisableAllPlayerActions()
+	{
+		Instance.DisableAllPlayer();
+	}
+	public static void EnableAllActions()
+	{
+		Instance.EnableAll();
+	}
+	public static void DisableAllActions()
+	{
+		Instance.DisableAll();
+	}
+
+	public static void SubscribeToPerformed(Player input, Action<InputAction.CallbackContext> function)
+	{
+		Instance.SubscribeOnPerform(input, function);
+	}
+	public static void UnsubscribeFromPerformed(Player input, Action<InputAction.CallbackContext> function)
+	{
+		Instance.UnsubscribeOnPerform(input, function);
+	}
+	public static void SubscribeToCancelled(Player input, Action<InputAction.CallbackContext> function)
+	{
+		Instance.SubscribeOnCancel(input, function);
+	}
+
+	public static void UnsubscribeFromCancelled(Player input, Action<InputAction.CallbackContext> function)
+	{
+		Instance.UnsubscribeOnCancel(input, function);
+	}
+	#endregion
+	#endregion
+
+	#region Device Switch
+	private void SetupDeviceType()
+	{
+		//Subscribe to all the inputaction.started to know what the last input was
+		foreach (var action in _playerInputActions)
+		{
+			action.started += ListenToDeviceType;
+		}
+	}
+
+	private void ListenToDeviceType(InputAction.CallbackContext context)
+	{
+		//Check the context to see if the input was from a controller or keyboard
+		_lastInputWasController = context.control.device is Gamepad or Joystick;
+
+		if (!_lastInputWasController)
+		{
+			CurrentDeviceType = InputType.Keyboard;
+			return;
+		}
+
+		CurrentDeviceType = InputType.GamePad;
+	}
+
+	private void OnDeviceSwitch(InputType type)
+	{
+		var handler = DeviceSwitch;
+		handler?.Invoke(this, new DeviceSwitchEventArgs(type));
+	}
+}
+public class DeviceSwitchEventArgs : EventArgs
+{
+	public CustomInputManager.InputType Type { get; private set; }
+
+	public DeviceSwitchEventArgs(CustomInputManager.InputType type)
+	{
+		Type = type;
+	}
+}
+#endregion
---
+++ GGJ2026/Assets/#Project/Scripts/PlayerMovement3D.cs
@@ -0,0 +1,220 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement3D : MonoBehaviour
+{
+	#region Editor Fields
+	[Header("Components")]
+	[SerializeField] private Rigidbody _rb;
+
+	[Header("Parameters")]
+	[SerializeField] private float _speed = 35f;
+	[SerializeField] private float _rotationSpeed = 10f;
+	[SerializeField] private float _groundDrag = 5f;
+	[SerializeField] private float _airSpeed = 8;
+	[SerializeField] private float _airDrag = 5f;
+	[SerializeField] private float _gravityForce = -9.8f;
+
+	[Header("Grounded Checks")]
+	[SerializeField] private float _groundedCheckUpOffset;
+	[SerializeField] private float _groundedCheckRadius;
+	#endregion
+
+	#region Fields
+	private Transform _cameraPivot;
+	private InputAction _moveInput;
+	private Vector3 _inputDirection;
+	private Vector3 _lookDirection;
+	private bool _isGrounded;
+
+	private RaycastHit[] _groundedResults = new RaycastHit[1];
+	private bool _canMove = true;
+	#endregion
+
+	#region Properties
+	public float SpeedModifier { get; set; } = 1;
+	public float AirSpeedModifier { get; set; } = 1;
+	public float JumpForceModifier { get; set; } = 1;
+	public float GravityModifier { get; set; } = 1;
+	public float GroundDragModifier { get; set; } = 1;
+	public float AirDragModifier { get; set; } = 1;
+
+	public Vector3 Velocity => _rb.linearVelocity;
+	public Vector3 InputDirection => _inputDirection.normalized;
+
+	public bool IsGrounded
+	{
+		get => _isGrounded;
+		private set
+		{
+			// prevent the same value from being passed through
+			if (_isGrounded == value) return;
+			// set the new grounded value
+			_isGrounded = value;
+		}
+	}
+
+	public bool UseGravity { get; set; } = true;
+	public bool CanMove
+	{
+		get => _canMove;
+		set
+		{
+			if (_canMove != value)
+			{
+				_canMove = value;
+			}
+		}
+	}
+
+	public bool CanRotate { get; set; } = true;
+
+	public Rigidbody Rigidbody => _rb;
+	#endregion
+
+	#region Events
+	#endregion
+
+	#region Mono
+	private void OnEnable()
+	{
+		_moveInput = CustomInputManager.GetInputAction(CustomInputManager.Player.Move);
+		_cameraPivot = Camera.main.transform;
+	}
+
+	private void OnDisable()
+	{
+		_moveInput = null;
+		_cameraPivot = null;
+	}
+	private void FixedUpdate()
+	{
+		// sets the rayscast position slightly above the feet of the player
+		Vector3 raycastPos = transform.position;
+		raycastPos.y += _groundedCheckUpOffset;
+
+		CalculateInputDirection();
+		CalculateDrag();
+		MovePlayer();
+		RotatePlayer();
+		CalculateIsGrounded(raycastPos);
+	}
+	#endregion
+
+	#region Methods
+	public void ResetMovementModifiers(bool clearExtraModifiers)
+	{
+		SpeedModifier = 1f;
+		AirSpeedModifier = 1f;
+		GravityModifier = 1f;
+		JumpForceModifier = 1f;
+		GroundDragModifier = 1f;
+		AirDragModifier = 1f;
+	}
+
+	private void CalculateInputDirection()
+	{
+		Vector2 input = _moveInput.ReadValue<Vector2>();
+		Vector3 camForward = Horizontal(_cameraPivot.forward);
+		Vector3 camRight = Horizontal(_cameraPivot.right);
+
+		_inputDirection = input.y * camForward + input.x * camRight;
+	}
+
+	private void CalculateDrag()
+	{
+		if (IsGrounded)
+		{
+			_rb.linearDamping = _groundDrag * GroundDragModifier;
+		}
+		else
+		{
+			_rb.linearDamping = _airDrag * AirDragModifier;
+		}
+	}
+	private void CalculateIsGrounded(Vector3 raycastPos)
+	{
+		// does the raycast to see if the player is grounded or not, if something on the layers got hit then the players is grounded
+		if (Physics.CheckSphere(raycastPos, _groundedCheckRadius, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore))
+		{
+			IsGrounded = true;
+			return;
+		}
+
+		IsGrounded = false;
+	}
+	private void MovePlayer()
+	{
+		Vector3 moveVector = _inputDirection;
+
+		if (!CanMove)
+		{
+			moveVector = Vector3.zero;
+		}
+
+		if (IsGrounded)
+		{
+			moveVector *= _speed * SpeedModifier;
+			moveVector.y = -1f;
+		}
+		else
+		{
+			moveVector *= _airSpeed * AirSpeedModifier;
+			if (UseGravity)
+			{
+				moveVector.y = _gravityForce * GravityModifier;
+			}
+		}
+
+		_rb.AddForce(moveVector, ForceMode.Acceleration);
+	}
+
+
+
+	private void RotatePlayer()
+	{
+		if (_inputDirection is not { x: 0f, z: 0f } && CanRotate)
+		{
+			_lookDirection = _inputDirection;
+		}
+
+		// if you can't rotate then you shouldn't do anything
+		if (!CanRotate) return;
+		if (_lookDirection.magnitude == 0) return;
+		Quaternion targetRotation = Quaternion.LookRotation(_lookDirection, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, _rotationSpeed);
+	}
+	public void SetVelocityToZero()
+	{
+		_rb.linearVelocity = Vector3.zero;
+	}
+
+	public void AddForce(Vector3 force, ForceMode mode = ForceMode.Acceleration)
+	{
+		_rb.AddForce(force, mode);
+	}
+
+	public void Lookat(Vector3 direction, bool resetInput = false, bool snapToDirection = false)
+	{
+		_lookDirection = direction;
+		if (resetInput)
+		{
+			_inputDirection = Vector3.zero;
+		}
+		if (snapToDirection)
+		{
+			transform.LookAt(transform.position + direction, Vector3.up);
+		}
+	}
+
+	private Vector3 Horizontal(Vector3 reference)
+	{
+		reference.y = 0;
+		reference.Normalize();
+		return reference;
+	}
+	#endregion
+}
+
